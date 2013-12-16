134e5a17b975a01b2875941fc509c452bc9bd218
ulfworsoe <ulf.worsoe@mosek.com>
2013-12-16 10:23:56 +0100

    testing build script

--- deps/build.jl
+++ deps/build.jl
@@ -2,41 +2,22 @@ using BinDeps
 
 @BinDeps.setup
 
-@unix_only begin
-  if WORD_SIZE == 32
-    libmosek = library_dependency("libmosek")
-  else
-    libmosek = library_dependency("libmosek64")
-  end
-end
-@windows_only begin
-  if WORD_SIZE == 32
-    libmosek = library_dependency("mosek7_0")
-  else
-    libmosek = library_dependency("mosek64_7_0")
-  end
-end
 
-@linux_only begin
-  if WORD_SIZE == 32
-    provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolslinux32x86.tar.bz2"), [libmosek])
-  else
-    provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolslinux64x86.tar.bz2"), [libmosek])
-  end
+if WORD_SIZE == 32
+  libmosek = library_dependency("libmosek", os = :Unix)
+  libmosek = library_dependency("mosek7_0", os = :Windows)
+  provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolslinux32x86.tar.bz2"), [libmosek], unpacked_dir="mosek", os = :Linux)
+  provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolswin32x86.zip"),       [libmosek], unpacked_dir="mosek", os = :Windows)
+else
+  libmosek = library_dependency("libmosek64", os = :Unix)
+  libmosek = library_dependency("mosek64_7_0", os = :Windows)
+  provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolslinux64x86.tar.bz2"), [libmosek], unpacked_dir="mosek", os = :Linux)
+  provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolswin64x86.zip"),       [libmosek], unpacked_dir="mosek", os = :Windows)
+  provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolsosx64x86.tar.bz2"),   [libmosek], unpacked_dir="mosek", os = :Darwin)
 end
+  
+print("HELLO! BinDeps script\n")
 
-@windows_only begin
-  if WORD_SIZE == 32
-    provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolswin32x86.zip"), [libmosek])
-  else
-    provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolswin64x86.zip"), [libmosek])
-  end
-end
 
-@osx_only begin 
-  provides(Binaries, URI("http://download.mosek.com/stable/7/mosektoolsosx64x86.tar.bz2"), [libmosek])
-end
-
-#provides(Binaries, joinpath("mosek","lib"), libmosek)
 
 @BinDeps.install
