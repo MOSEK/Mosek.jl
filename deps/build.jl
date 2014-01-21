a073c4fa4c82c77d1d17edb51aae0bf14b2cece3
uw <wfluro@gmail.com>
2014-01-21 11:40:57 +0100

    updated mosek installation procedure (testen on lnx 64x86)

--- deps/build.jl
+++ deps/build.jl
@@ -22,7 +22,7 @@ mskplatform,pfdlls,distroext =
 if haskey(ENV,"MOSEKBINDIR")
   provides(Binaries, ENV["MOSEKBINDIR"], libmosek)
 # 2. Otherwise, use the default installation path
-else haskey(ENV,"HOME") && isdir(joinpath(ENV["HOME"],"mosek","7","tools","platform",mskplatform))
+elseif haskey(ENV,"HOME") && isdir(joinpath(ENV["HOME"],"mosek","7","tools","platform",mskplatform))
   if WORD_SIZE == 32
     provides(Binaries, joinpath(ENV["HOME"],"mosek","7","tools","platform","linux32x86","bin"), libmosek, os = :Linux)
     provides(Binaries, joinpath(ENV["HOME"],"mosek","7","tools","platform","win32x86","bin"),   libmosek, os = :Windows)
@@ -32,34 +32,40 @@ else haskey(ENV,"HOME") && isdir(joinpath(ENV["HOME"],"mosek","7","tools","platf
     provides(Binaries, joinpath(ENV["HOME"],"mosek","7","tools","platform","win64x86","bin"),   libmosek, os = :Windows)
   end
 # 3. Otherwise, fetch the MOSEK distro and unpack it
-# else
-#   mskplatform,pfdlls,distroext =
-#     if WORD_SIZE == 32
-#       if     OS_NAME == :Linux "linux32x86",  "lib*.so*", ".tar.bz2"
-#       elseif OS_NAME == :Windows "win32x86",  "*.dll",    ".zip"
-#       else   error("Platform not supported")                         
-#       end                                   
-#     else                                    
-#       if     OS_NAME == :Linux   "linux64x86","lib*.so*", ".tar.bz2"
-#       elseif OS_NAME == :Darwin  "osx4x86",   "*.dylib",  ".tar.bz2"
-#       elseif OS_NAME == :Windows "win64x86",  "*.dll",    ".zip"
-#       else   error("Platform not supported")
-#       end
-#     end
-# 
-#   copycmd = if OS_NAME == :Windows "copy" else "cp" end
-#   srcdir  = joinpath(BinDeps.depsdir(libmosek),"src")
-#   tarname = string("mosektools",mskplatform, distroext)
-#   prefix  = joinpath(BinDeps.depsdir(libmosek),"usr")
-# 
-#   provides(Sources, URI(string("http://download.mosek.com/stable/7/",tarname)), libmosek, unpacked_dir="mosek")
-# 
-#   provides(SimpleBuild
-#     ( @build_steps begin
-#         GetSources(libmosek)
-#         `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$pfdlls" "$prefix/lib"`
-#       end),
-#       libmosek)
+else
+  mskplatform,pfdlls,distroext =
+    if WORD_SIZE == 32
+      if     OS_NAME == :Linux "linux32x86",  ["libmosek.so",       "libmosek.so.7.0",     "libiomp5.so"],   ".tar.bz2"
+      elseif OS_NAME == :Windows "win32x86",  ["mosek_7_0.dll",     "mosek_7_0.dll",       "libiomp5.dll" ], ".zip"
+      else   error("Platform not supported")                         
+      end                                   
+    else                                    
+      if     OS_NAME == :Linux   "linux64x86",["libmosek64.so",     "libmosek64.so.7.0",   "libiomp5.so"], ".tar.bz2"
+      elseif OS_NAME == :Darwin  "osx4x86",   ["libmosek64.dylib",  "libmosek64.7.0.dylib","libiomp5.dylib"],  ".tar.bz2"
+      elseif OS_NAME == :Windows "win64x86",  ["libmosek64_7_0.dll","libmosek64_7_0.dll",  "libiomp5.dll"],    ".zip"
+      else   error("Platform not supported")
+      end
+    end
+
+  copycmd = if OS_NAME == :Windows "copy" else "cp" end
+  srcdir  = joinpath(BinDeps.depsdir(libmosek),"src")
+  tarname = string("mosektools",mskplatform, distroext)
+  prefix  = joinpath(BinDeps.depsdir(libmosek),"usr")
+
+  provides(Sources, URI(string("http://download.mosek.com/stable/7/",tarname)), libmosek, unpacked_dir="mosek")
+
+  provides(SimpleBuild
+    ( @build_steps begin
+        GetSources(libmosek)
+        CreateDirectory(joinpath(BinDeps.depsdir(libmosek),"usr","lib"))
+        ( @build_steps begin
+          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[2])" "$prefix/lib"`
+          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[3])" "$prefix/lib"`
+          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[1])" "$prefix/lib"`
+          end
+        )
+      end),
+      libmosek)
 end
 
 @BinDeps.install
