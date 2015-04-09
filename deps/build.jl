c92585692248d7c0c0490d59ab50e51cd0864492
ulfw <ulf.worsoe@mosek.com>
2015-04-09 13:39:01 +0200

    removing all things BinDeps from installtion

--- deps/build.jl
+++ deps/build.jl
@@ -1,81 +1,89 @@
-using BinDeps
+include("liftedfrombindeps.jl")
 
-@BinDeps.setup
+# define current version:
+mskvmajor = "7"
+mskvminor = "1"
 
-libmosek = library_dependency("libmosek", aliases=["libmosek64","mosek7_0","mosek64_7_0"])
-
-mskplatform,pfdlls,distroext,libmosekname =
+mskplatform,distroext =
   if WORD_SIZE == 32
-    if     OS_NAME == :Linux "linux32x86",  ["libmosek.so",       "libmosek.so.7.0",     "libiomp5.so",    "libmosekglb.so.7.0"],   ".tar.bz2", "libmosek"
-    elseif OS_NAME == :Windows "win32x86",  ["mosek_7_0.dll",     "mosek_7_0.dll",       "libiomp5md.dll", "mosekglb_7_0"        ], ".zip",     "mosek7_0"
-    else   error("Platform not supported")                         
+    if     OS_NAME == :Linux "linux32x86",  ".tar.bz2"
+    elseif OS_NAME == :Windows "win32x86",  ".zip"
+    else   error("Platform not supported")
     end                                   
   else                                    
-    if     OS_NAME == :Linux   "linux64x86",["libmosek64.so",     "libmosek64.so.7.0",   "libiomp5.so",    "libmosekglb64.so.7.0"],    ".tar.bz2", "libmosek64"
-    elseif OS_NAME == :Darwin  "osx64x86",  ["libmosek64.dylib",  "libmosek64.7.0.dylib","libiomp5.dylib", "libmosekglb64.7.0.dylib"], ".tar.bz2", "libmosek64"
-    elseif OS_NAME == :Windows "win64x86",  ["libmosek64_7_0.dll","libmosek64_7_0.dll",  "libiomp5md.dll", "mosekglb64_7_0.dll"],      ".zip",     "moske64_7_0"
+    if     OS_NAME == :Linux   "linux64x86",".tar.bz2"
+    elseif OS_NAME == :Darwin  "osx64x86",  ".tar.bz2"
+    elseif OS_NAME == :Windows "win64x86",  ".zip"
     else   error("Platform not supported")
     end
   end
 
-# 1. Is MOSEKBINDIR set? If so this must point to the binaries dir in the MOSEK DISTRO  
-if haskey(ENV,"MOSEKBINDIR")
-  provides(Binaries, ENV["MOSEKBINDIR"], libmosek)
-# 2. Otherwise, use the default installation path
-elseif haskey(ENV,"HOME") && isdir(joinpath(ENV["HOME"],"mosek","7","tools","platform",mskplatform))
-  if WORD_SIZE == 32
-    provides(Binaries, joinpath(ENV["HOME"],"mosek","7","tools","platform","linux32x86","bin"), libmosek, os = :Linux)
-    provides(Binaries, joinpath(ENV["HOME"],"mosek","7","tools","platform","win32x86","bin"),   libmosek, os = :Windows)
-  else
-    provides(Binaries, joinpath(ENV["HOME"],"mosek","7","tools","platform","linux64x86","bin"), libmosek, os = :Linux)
-    provides(Binaries, joinpath(ENV["HOME"],"mosek","7","tools","platform","osx64x86","bin"),   libmosek, os = :Darwin)
-    provides(Binaries, joinpath(ENV["HOME"],"mosek","7","tools","platform","win64x86","bin"),   libmosek, os = :Windows)
+libalternatives = 
+  if     mskplatform == "linux32x86" 
+                                       [ ("libmosek.so.7.0",     "libmosekscopt7_0.so"),    ("libmosek.so.7.1",     "libmosekscopt7_1.so") ]
+  elseif mskplatform == "linux64x86" 
+                                       [ ("libmosek64.so.7.0",   "libmosekscopt7_0.so"),    ("libmosek64.so.7.1",   "libmosekscopt7_1.so") ]
+  elseif mskplatform == "osx64x86"   
+                                       [ ("libmosek64.7.0.dylib","libmosekscopt7_0.dylib"), ("libmosek64.7.1.dylib","libmosekscopt7_1.dylib") ]
+  elseif mskplatform == "win32x86"   
+                                       [ ("mosek7_0.dll",        "mosekscopt7_0.dll"),      ("mosek7_1.dll",        "mosekscopt7_1.dll") ]
+  elseif mskplatform == "win64x86"   
+                                       [ ("mosek64_7_0.dll",     "mosekscopt7_0.dll"),      ("mosek64_7_1.dll",     "mosekscopt7_1.dll") ]
+  else   error("Platform not supported")
   end
-elseif haskey(ENV,"HOMEDRIVE") && haskey(ENV,"HOMEPATH") && isdir(joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"]),"mosek","7","tools","platform",mskplatform))
-  home = string(ENV["HOMEDRIVE"],ENV["HOMEPATH"])
-  if WORD_SIZE == 32
-    provides(Binaries, joinpath(home,"mosek","7","tools","platform","linux32x86","bin"), libmosek, os = :Linux)
-    provides(Binaries, joinpath(home,"mosek","7","tools","platform","win32x86","bin"),   libmosek, os = :Windows)
-  else
-    provides(Binaries, joinpath(home,"mosek","7","tools","platform","linux64x86","bin"), libmosek, os = :Linux)
-    provides(Binaries, joinpath(home,"mosek","7","tools","platform","osx64x86","bin"),   libmosek, os = :Darwin)
-    provides(Binaries, joinpath(home,"mosek","7","tools","platform","win64x86","bin"),   libmosek, os = :Windows)
-  end
-# 3. Otherwise, fetch the MOSEK distro and unpack it
-else
-  copycmd = if OS_NAME == :Windows "copy" else "cp" end
-  srcdir  = joinpath(BinDeps.depsdir(libmosek),"src")
-  tarname = string("mosektools",mskplatform, distroext)
-  prefix  = joinpath(BinDeps.depsdir(libmosek),"usr")
 
-  provides(Sources, URI(string("http://download.mosek.com/stable/7/",tarname)), libmosek, unpacked_dir="mosek")
-  provides(SimpleBuild,
-    ( @build_steps begin
-        GetSources(libmosek)
-        CreateDirectory(joinpath(BinDeps.depsdir(libmosek),"usr","lib"))
-        ( @build_steps begin
-          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[2])" "$prefix/lib"`
-          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[3])" "$prefix/lib"`
-          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[4])" "$prefix/lib"`
-          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[1])" "$prefix/lib"`
-          end
-        )
-      end),
-      libmosek,
-      os = :Unix)
-  provides(SimpleBuild,
-    ( @build_steps begin
-        GetSources(libmosek)
-        CreateDirectory(joinpath(BinDeps.depsdir(libmosek),"usr","lib"))
-        ( @build_steps begin
-          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[2])" "$prefix/lib"`
-          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[3])" "$prefix/lib"`
-          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[4])" "$prefix/lib"`
-          `$copycmd "$srcdir/mosek/7/tools/platform/$mskplatform/bin/$(pfdlls[1])" "$prefix/lib"`
-          end
-        )
-      end),
-      libmosek,
-      os = :Windows)
+bindepsdir = dirname(@__FILE__)
+     
+mskbindir = 
+# 1. Is MOSEKBINDIR set? If so this must point to the binaries dir in the MOSEK DISTRO
+    if haskey(ENV,"MOSEKBINDIR")
+        ENV["MOSEKBINDIR"],idxs
+    elseif haskey(ENV,"MOSEK_7_1_BINDIR")
+        ENV["MOSEK_7_1_BINDIR"]
+# 2a. Otherwise, use the default installation path (Linux)
+    elseif haskey(ENV,"HOME") && isdir(joinpath(ENV["HOME"],"mosek","7","tools","platform",mskplatform))
+        joinpath(ENV["HOME"],"mosek","7","tools","platform",mskplatform,"bin")
+# 2b. Windows default install path
+    elseif ( haskey(ENV,"HOMEDRIVE") && 
+            haskey(ENV,"HOMEPATH") && 
+            isdir(joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"]),"mosek","7","tools","platform",mskplatform)) )
+        home = string(ENV["HOMEDRIVE"],ENV["HOMEPATH"])
+        joinpath(home,"mosek","7","tools","platform",mskplatform,"bin")
+# 3. Otherwise, fetch the MOSEK distro and unpack it
+    else
+        srcdir  = joinpath(bindepsdir,"src")
+        dldir   = joinpath(bindepsdir,"downloads")
+        tarname = string("mosektools",mskplatform, distroext)
+        
+        basename,ext,sndext = splittarpath(tarname)
+
+        mkpath(dldir)
+        info("Download MOSEK distro (http://download.mosek.com/stable/7/$tarname)")
+        success(download_cmd(string("http://download.mosek.com/stable/7/",tarname), joinpath(dldir,tarname))) || error("Failed to download MOSEK distro")
+        mkpath(srcdir)
+        info("Unpack MOSEK distro ($dldir/$tarname -> $srcdir)")
+        success(unpack_cmd(joinpath(dldir,tarname),srcdir, ext, sndext)) || error("Failed to unpack MOSEK distro")
+
+        joinpath(srcdir,"mosek","7","tools","platform",mskplatform,"bin")
+    end
+
+idxs = reverse(find(libs -> all(lib -> isfile(joinpath(mskbindir,lib)), libs), libalternatives))
+if length(idxs) == 0 
+    error("Failed to find any usable MOSEK libraries")
+else
+    libmosek,libmosekscopt = libalternatives[idxs[1]]
+
+    f = open(joinpath(bindepsdir,"deps.jl"),"w")
+    write(f,"""# This is an auto-generated file; do not edit
+# Macro to load a library
+macro checked_lib(libname, path)
+    (dlopen_e(path) == C_NULL) && error("Unable to load \\n\\n\$libname (\$path)\\n\\nPlease re-run Pkg.build(package), and restart Julia.")
+    quote const \$(esc(libname)) = \$path end
 end
-@BinDeps.install [ :libmosek => :libmosek ]
+
+# Load dependencies
+@checked_lib libmosek      "$mskbindir/$libmosek"
+@checked_lib libmosekscopt "$mskbindir/$libmosekscopt"
+""")
+    close(f)
+end
---
+++ deps/liftedfrombindeps.jl
@@ -0,0 +1,71 @@
+# Blatantly lifted from BinDeps.jl because for some reason the build
+# script sometimes cannot find BinDeps.
+
+function splittarpath(path) 
+    path,extension = splitext(path)
+    base_filename,secondary_extension = splitext(path)
+    if extension == ".tgz" || extension == ".tbz" || extension == ".zip" && !isempty(secondary_extension)
+        base_filename *= secondary_extension
+        secondary_extension = ""
+    end
+    (base_filename,extension,secondary_extension)
+end
+
+downloadcmd = nothing
+function download_cmd(url::String, filename::String)
+    global downloadcmd
+    if downloadcmd === nothing
+        for checkcmd in @windows? (:powershell, :curl, :wget, :fetch) : (:curl, :wget, :fetch)
+            try
+                if success(`$checkcmd --help`)
+                    downloadcmd = checkcmd
+                    break
+                end
+            catch
+                continue # don't bail if one of these fails
+            end
+        end
+    end
+    if downloadcmd == :wget
+        return `wget -O $filename $url`
+    elseif downloadcmd == :curl
+        return `curl -o $filename -L $url`
+    elseif downloadcmd == :fetch
+        return `fetch -f $filename $url`
+    elseif downloadcmd == :powershell
+        return `powershell -Command "(new-object net.webclient).DownloadFile(\"$url\", \"$filename\")"`
+    else
+        error("No download agent available; install curl, wget, or fetch.")
+    end
+end
+
+@unix_only begin
+    function unpack_cmd(file,directory,extension,secondary_extension)
+        if (extension == ".gz" && secondary_extension == ".tar") || extension == ".tgz"
+            return (`tar xzf $file --directory=$directory`)
+        elseif (extension == ".bz2" && secondary_extension == ".tar") || extension == ".tbz"
+            return (`tar xjf $file --directory=$directory`)
+        elseif extension == ".xz" && secondary_extension == ".tar"
+            return (`unxz -c $file `|>`tar xv --directory=$directory`)
+        elseif extension == ".tar"
+            return (`tar xf $file --directory=$directory`)
+        elseif extension == ".zip"
+            return (`unzip -x $file -d $directory`)
+        elseif extension == ".gz"
+            return (`mkdir $directory` |> `cp $file $directory` |> `gzip -d $directory/$file`)
+        end
+        error("I don't know how to unpack $file")
+    end
+end
+
+@windows_only begin
+    function unpack_cmd(file,directory,extension,secondary_extension)
+        if((extension == ".gz" || extension == ".xz" || extension == ".bz2") && secondary_extension == ".tar") ||
+            extension == ".tgz" || extension == ".tbz"
+            return (`7z x $file -y -so`|>`7z x -si -y -ttar -o$directory`)
+        elseif extension == ".zip" || extension == ".7z"
+            return (`7z x $file -y -o$directory`)
+        end
+            error("I don't know how to unpack $file")
+        end 
+    end
