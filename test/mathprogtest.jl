ec81954ed66a9548ca659cced2e342bfebad67a7
ulfw <ulf.worsoe@mosek.com>
2015-11-27 13:39:54 +0100

    implemented MosekLinearQuadraticModel

--- test/mathprogtest.jl
+++ test/mathprogtest.jl
@@ -5,8 +5,8 @@ using MathProgBase.SolverInterface
 include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
 include(joinpath(Pkg.dir("MathProgBase"),"test","quadprog.jl"))
 include(joinpath(Pkg.dir("MathProgBase"),"test","mixintprog.jl"))
-include(joinpath(Pkg.dir("MathProgBase"),"test","nlp.jl"))
 include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
+include(joinpath(Pkg.dir("MathProgBase"),"test","nlp.jl"))
 
 
 
