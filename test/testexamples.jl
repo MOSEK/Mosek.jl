fabdc4a0f7e537cb519a0bfc3fcb54879892b323
ulfw <ulf.worsoe@mosek.com>
2018-05-17 14:03:13 +0200

    improve test

--- test/testexamples.jl
+++ test/testexamples.jl
@@ -1,14 +1,44 @@
-include("../examples/lo1.jl")
-include("../examples/cqo1.jl")
-include("../examples/qo1.jl")
-include("../examples/qcqo1.jl")
-include("../examples/milo1.jl")
-include("../examples/sdo1.jl")
-include("../examples/eo1.jl")
-include("../examples/solvebasis.jl")
-include("../examples/portfolio.jl")
-include("../examples/production.jl")
-include("../examples/callback.jl")
+@testset "[distro examples]" begin
+    @testset "lo1" begin
+        include("../examples/lo1.jl")
+    end
+    @testset "cqo1" begin
+        include("../examples/cqo1.jl")
+    end
+    @testset "qo1" begin
+        include("../examples/qo1.jl")
+    end
+    @testset "qcqo1" begin
+        include("../examples/qcqo1.jl")
+    end
+    @testset "milo1" begin
+        include("../examples/milo1.jl")
+    end
+    @testset "sdo1" begin
+        include("../examples/sdo1.jl")
+    end
+    @testset "eo1" begin
+        include("../examples/eo1.jl")
+    end
+    @testset "pow1" begin
+        include("../examples/pow1.jl")
+    end
+    @testset "solvebasis" begin
+        include("../examples/solvebasis.jl")
+    end
+    @testset "portfolio" begin
+        include("../examples/portfolio.jl")
+    end
+    @testset "production" begin
+        include("../examples/production.jl")
+    end
+    @testset "callback" begin
+        include("../examples/callback.jl")
 
-push!(ARGS, "$(dirname(dirname(@__FILE__)))/examples/feasrepair.lp")
-include("../examples/feasrepairex1.jl")
+    end
+    @testset "feasrepairex1" begin
+        push!(ARGS, "$(dirname(dirname(@__FILE__)))/examples/feasrepair.lp")
+        include("../examples/feasrepairex1.jl")
+
+    end
+end
